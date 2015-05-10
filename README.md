26c58adbc3c3d1865bf16b3f00bb6cbc67548413
glennpackerBV <glenn.packer@bevalued.co.uk>
2015-05-10 18:54:39 +0100

    Initial
    FB Login checked
    Default CSS removed and Bootstrap Added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # meetup
+
+
+This website uses the meetup api calls to create your own meetup website.
+
+-----------------------
+Setup your own web.config there is a demo one to give you the idea.
+pay attentions to the appSettings keys where you will need to provide your facebook application id and secret (https://developers.facebook.com) along with the meetup key.
+
---
+++ src/MeetUp/MeetUp.Web/App_Start/AuthConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using MeetUp.Web.Models;
+
+namespace MeetUp.Web
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                appId: ConfigurationManager.AppSettings["FaceBookAppId"],
+                appSecret: ConfigurationManager.AppSettings["FaceBookAppSecret"]);
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}

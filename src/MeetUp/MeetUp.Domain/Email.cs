b87a7d7ec3c5225e4448e086abc644f3311e13a1
glenn packer <glenn.packer@yahoo.co.uk>
2015-06-03 12:17:25 +0100

    email class change

--- src/MeetUp/MeetUp.Domain/Email.cs
+++ src/MeetUp/MeetUp.Domain/Email.cs
@@ -6,15 +6,15 @@ namespace MeetUp.Domain
     public class Email
     {
         [Key]
-        public int FromUserId { get; set; }
-        public int ToUserId { get; set; }
-        public string EmailAddress { get; set; }
-        public string MessageType { get; set; }
+		public int Id { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
 
-
+		public int ToUserId { get; set; }
         [ForeignKey("ToUserId")]
         public virtual UserAccount ToUser { get; set; }
 
+		public int FromUserId { get; set; }
         [ForeignKey("FromUserId")]
         public virtual UserAccount FromUser { get; set; }
 
